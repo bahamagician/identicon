b9f28cd45cafd2ca597868bc0ea3c1c8dc64c78d
SC Graphics <scgraphics@SCs-iMac.local>
2018-12-24 15:33:00 -0500

    implement build grid method

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,11 +3,22 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_colour
+    |> build_grid
+    |> Enum.map(&mirror_row/1)
   end
 
-  def pick_colour(image) do
-    %Identicon.Image{hex: [r, g, b | _tail]} = image
-    [r, g, b]
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk(3)
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
+  end
+
+  def pick_colour(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
   end
 
   def hash_input(input) do
